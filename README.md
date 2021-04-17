4dbdd207d53eafc1d93d27601c410263b7d60a46
Matthew Phillips <matthew.phillips@driveclutch.com>
2021-04-17 18:26:12 -0400

    update
    update
    
    update
    
    update
    
    update
    
    update
    
    update
    
    update
    
    update
    
    update
    
    update
    
    update
    
    update
    
    update
    
    update
    
    update
    
    update
    
    update
    
    update
    
    update
    
    update
    
    update
    
    update
    
    update

---
+++ README.md
@@ -0,0 +1,68 @@
+# maxmind_geo_ip2 API client
+
+Still very much a work in progress - this   
+
+Todos: 
+- error handling
+- response status checks
+
+## Dependencies
+````
+reqwest = { version = "0.11.3", features = ["rustls", "json"] }
+serde = { version = "1.0.125", features = ["derive"] }
+````
+
+## Usage
+
+Import the api client from the crate:
+
+````
+use maxmind_geo_ip2::ApiClient;
+````
+
+Initialize the client with Maxmind API credentials: 
+
+````
+let maxmind_client = ApiClient.new_client(String::from("user_id_here"), String::from("license_key_here"));
+````
+
+## Methods
+
+### Get Country
+
+API endpoint: 
+````
+https://geoip.maxmind.com/geoip/v2.1/country/{ip_address}
+````
+
+Method signature:
+````
+get_country(ip_address: String) -> Result<Response, Box<dyn std::error::Error>>
+````
+
+
+### Get City
+
+API endpoint:
+````
+https://geoip.maxmind.com/geoip/v2.1/city/{ip_address}
+````
+
+Method signature:
+````
+get_city(ip_address: String) -> Result<Response, Box<dyn std::error::Error>>
+````
+
+
+### Get Insights
+
+API endpoint:
+````
+https://geoip.maxmind.com/geoip/v2.1/insights/{ip_address}
+````
+
+
+Method signature:
+````
+get_insights(ip_address: String) -> Result<Response, Box<dyn std::error::Error>>
+````
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 
-struct ApiClient {
+pub struct ApiClient {
     user_id: String,
     license_key: String,
     client: reqwest::Client,
@@ -16,15 +16,16 @@ impl ApiClient {
         }
     }
 
-    #[tokio::main]
     async fn make_request(
         &self,
         prefix: String,
         ip_address: String,
     ) -> Result<Response, Box<dyn std::error::Error>> {
-        let url = format!("{}/{}", prefix, ip_address);
+        let url = format!("{}{}", prefix, ip_address);
 
-        let resp = self.client.get(url)
+        let resp = self
+            .client
+            .get(url)
             .basic_auth(&self.user_id, Some(&self.license_key))
             .send()
             .await?
@@ -33,51 +34,64 @@ impl ApiClient {
         Ok(resp)
     }
 
-    pub fn get_country(&self, ip_address: String) -> Result<Response, Box<dyn std::error::Error>> {
+    pub async fn get_country(
+        &self,
+        ip_address: String,
+    ) -> Result<Response, Box<dyn std::error::Error>> {
         self.make_request(
             String::from("https://geoip.maxmind.com/geoip/v2.1/country/"),
             ip_address,
         )
+        .await
     }
 
-    pub fn get_city(&self, ip_address: String) -> Result<Response, Box<dyn std::error::Error>> {
+    pub async fn get_city(
+        &self,
+        ip_address: String,
+    ) -> Result<Response, Box<dyn std::error::Error>> {
         self.make_request(
             String::from("https://geoip.maxmind.com/geoip/v2.1/city/"),
             ip_address,
         )
+        .await
     }
 
-    pub fn get_insights(&self, ip_address: String) -> Result<Response, Box<dyn std::error::Error>> {
+    pub async fn get_insights(
+        &self,
+        ip_address: String,
+    ) -> Result<Response, Box<dyn std::error::Error>> {
         self.make_request(
             String::from("https://geoip.maxmind.com/geoip/v2.1/insights/"),
             ip_address,
         )
+        .await
     }
 }
 
-#[derive(serde::Deserialize)]
+#[derive(serde::Deserialize, Debug)]
 pub struct City {
     pub confidence: Option<i64>,
-    pub geno_name_id: Option<i64>,
-    pub names: Option<HashMap<String, String>>
+    pub geoname_id: Option<i64>,
+    pub names: Option<HashMap<String, String>>,
 }
 
-#[derive(serde::Deserialize)]
+#[derive(serde::Deserialize, Debug)]
 pub struct Continent {
     pub code: Option<String>,
-    pub geno_name_id: Option<i64>,
-    pub names: Option<HashMap<String, String>>
+    pub geoname_id: Option<i64>,
+    pub names: Option<HashMap<String, String>>,
 }
 
-#[derive(serde::Deserialize)]
+#[derive(serde::Deserialize, Debug)]
 pub struct Country {
     pub confidence: Option<i64>,
-    pub geo_name_id: Option<i64>,
+    pub geoname_id: Option<i64>,
+    pub is_in_european_union: Option<bool>,
     pub iso_code: Option<String>,
-    pub names: Option<HashMap<String, String>>
+    pub names: Option<HashMap<String, String>>,
 }
 
-#[derive(serde::Deserialize)]
+#[derive(serde::Deserialize, Debug)]
 pub struct Location {
     pub accuracy_radius: Option<i64>,
     pub average_income: Option<i64>,
@@ -85,57 +99,67 @@ pub struct Location {
     pub longitude: Option<f64>,
     pub metro_code: Option<i64>,
     pub population_density: Option<i64>,
-    pub time_zone: Option<String>
+    pub time_zone: Option<String>,
 }
 
-#[derive(serde::Deserialize)]
-pub struct Postal{
+#[derive(serde::Deserialize, Debug)]
+pub struct Postal {
     pub code: Option<String>,
-    pub confidence: Option<i64>
+    pub confidence: Option<i64>,
 }
 
-#[derive(serde::Deserialize)]
+#[derive(serde::Deserialize, Debug)]
 pub struct RegisteredCountry {
-    pub geo_name_id: Option<i64>,
-    pub iso_code: Option<String>,
-    pub names: Option<HashMap<String, String>>
-}
-
-#[derive(serde::Deserialize)]
-pub struct RepresentedCountry {
-    pub geo_name_id: Option<i64>,
+    pub geoname_id: Option<i64>,
+    pub is_in_european_union: Option<bool>,
     pub iso_code: Option<String>,
     pub names: Option<HashMap<String, String>>,
-    pub r#type: Option<String>
 }
 
-#[derive(serde::Deserialize)]
+#[derive(serde::Deserialize, Debug)]
+pub struct RepresentedCountry {
+    pub geoname_id: Option<i64>,
+    pub iso_code: Option<String>,
+    pub names: Option<HashMap<String, String>>,
+    pub r#type: Option<String>,
+}
+
+#[derive(serde::Deserialize, Debug)]
 pub struct Subdivision {
     pub confidence: Option<i64>,
-    pub geo_name_id: Option<i64>,
+    pub geoname_id: Option<i64>,
     pub iso_code: Option<String>,
-    pub names: Option<HashMap<String, String>>
+    pub names: Option<HashMap<String, String>>,
 }
 
-#[derive(serde::Deserialize)]
+#[derive(serde::Deserialize, Debug)]
 pub struct Traits {
     pub autonomous_system_number: Option<i64>,
     pub autonomous_system_organization: Option<String>,
     pub domain: Option<String>,
+    pub is_anonymous: Option<bool>,
     pub is_anonymous_proxy: Option<bool>,
+    pub is_anonymous_vpn: Option<bool>,
+    pub is_hosting_provider: Option<bool>,
+    pub is_public_proxy: Option<bool>,
+    pub is_residential_proxy: Option<bool>,
     pub is_satellite_provider: Option<bool>,
+    pub is_tor_exit_node: Option<bool>,
     pub isp: Option<String>,
+    pub network: Option<String>,
     pub ip_address: Option<String>,
     pub organization: Option<String>,
-    pub user_type: Option<String>
+    pub static_ip_score: Option<f64>,
+    pub user_count: Option<i64>,
+    pub user_type: Option<String>,
 }
 
-#[derive(serde::Deserialize)]
+#[derive(serde::Deserialize, Debug)]
 pub struct MaxMind {
-    pub queries_remaining: Option<i64>
+    pub queries_remaining: Option<i64>,
 }
 
-#[derive(serde::Deserialize)]
+#[derive(serde::Deserialize, Debug)]
 pub struct Response {
     city: Option<City>,
     continent: Option<Continent>,
@@ -146,5 +170,5 @@ pub struct Response {
     represented_country: Option<RepresentedCountry>,
     subdivisions: Option<Vec<Subdivision>>,
     traits: Option<Traits>,
-    maxmind: Option<MaxMind>
+    maxmind: Option<MaxMind>,
 }
